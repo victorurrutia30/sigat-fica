67573b8e863ec53e5707adfb0bfa4b0141a71fa2
Victor <tu-email@example.com>
2026-05-08 13:19:16 -0600

    style: configurar paleta UTEC en Tailwind

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+? 'inline-flex items-center px-1 pt-1 border-b-2 border-utec-primary text-sm font-medium leading-5 text-utec-primary focus:outline-none focus:border-utec-primary-dark transition duration-150 ease-in-out'
+: 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-600 hover:text-utec-primary hover:border-utec-primary-light focus:outline-none focus:text-utec-primary focus:border-utec-primary-light transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
